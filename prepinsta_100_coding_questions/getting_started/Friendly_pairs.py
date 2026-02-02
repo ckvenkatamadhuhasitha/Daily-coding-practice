4e435bdfa55f3fa9073b95ad76d6e31d0d1cfd26
C K VENKATA MADHU HASITHA <153090038+ckvenkatamadhuhasitha@users.noreply.github.com>
2026-02-02 22:56:40 +0530

    Add files via upload

---
+++ prepinsta_100_coding_questions/getting_started/Friendly_pairs.py
@@ -0,0 +1,34 @@
+# Problem: Friendly Pairs
+# Example: (6, 28)
+# Factors of 6  → 1, 2, 3  → sum = 6
+# Factors of 28 → 1, 2, 4, 7, 14 → sum = 28
+# sum1 / num1 == sum2 / num2 → Friendly Pairs
+
+# -----------------------------
+# Optimized Approach
+# -----------------------------
+# Time Complexity: O(√n + √m)
+# Space Complexity: O(1)
+
+num1 = int(input('Enter a number: '))
+num2 = int(input('Enter another number: '))
+sum1, sum2 = 0, 0
+
+for i in range(1, (int(num1**0.5)) + 1):
+    if num1 % i == 0:
+        if i != num1:
+            sum1 = sum1 + i
+        if num1 // i != i and num1 // i != num1:
+            sum1 = sum1 + (num1 // i)
+
+for i in range(1, (int(num2**0.5)) + 1):
+    if num2 % i == 0:
+        if i != num2:
+            sum2 = sum2 + i
+        if num2 // i != i and num2 // i != num2:
+            sum2 = sum2 + (num2 // i)
+
+if sum1 * num2 == sum2 * num1:
+    print('Friendly Pairs')
+else:
+    print('Not Friendly Pairs')
